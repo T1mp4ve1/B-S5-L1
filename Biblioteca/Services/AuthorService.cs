c57d0bee30c8f1a59cd0ee79898333fe97fc0eda
T1mp4ve1 <pavelwire@outlook.it>
2025-12-01 16:59:43 +0100

    author

---
+++ Biblioteca/Services/AuthorService.cs
@@ -0,0 +1,42 @@
+﻿using Biblioteca.Data;
+using Biblioteca.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteca.Services
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly AppDbContext _context;
+        public AuthorService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Author>> GetAllAsync()
+        {
+            return await _context.AuthorS.ToListAsync();
+        }
+        public async Task<Author?> GetByIdAsync(Guid id)
+        {
+            return await _context.AuthorS.FindAsync(id);
+        }
+        public async Task CreateAsync(Author author)
+        {
+            _context.AuthorS.Add(author);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(Author author)
+        {
+            _context.AuthorS.Update(author);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(Guid id)
+        {
+            var entity = await _context.AuthorS.FindAsync(id);
+            if (entity != null)
+            {
+                _context.AuthorS.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Biblioteca/Services/IAuthorService.cs
@@ -0,0 +1,13 @@
+﻿using Biblioteca.Models;
+
+namespace Biblioteca.Services
+{
+    public interface IAuthorService
+    {
+        Task<List<Author>> GetAllAsync();
+        Task<Author?> GetByIdAsync(Guid id);
+        Task CreateAsync(Author author);
+        Task UpdateAsync(Author author);
+        Task DeleteAsync(Guid id);
+    }
+}
